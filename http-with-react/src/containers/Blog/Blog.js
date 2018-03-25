ef2ef9f7185de47c07b64903416ec8f3befbbfea
nddv95 <nddv95@gmail.com>
2018-03-25 12:49:55 +0700

    finished

---
+++ http-with-react/src/containers/Blog/Blog.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import axios from 'axios';
+
+class Blog extends Component {
+
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPost = posts.map(post => {
+                    return {
+                        ...post,
+                        author:'Max'
+                    }
+                });
+                this.setState({posts:updatedPost});
+            }, err => {
+                this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        let posts = <p>Something when wrong</p>
+        if(!this.state.error){
+            posts = this.state.posts.map( post =>
+                <Post 
+                    key={post.id} 
+                    author={post.author} 
+                    title={post.title}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            );
+        }
+        
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section>
+                <section>
+                    <NewPost/>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
